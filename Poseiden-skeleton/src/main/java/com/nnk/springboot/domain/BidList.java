a390881158833d84dda508ae6f8502f7d91011e1
armandnjapou <armand.njapoungassu@consulting-for.accor.com>
2024-03-02 18:59:42 +0100

    setup project

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/BidList.java
@@ -1,15 +1,38 @@
 package com.nnk.springboot.domain;
 
-import org.springframework.beans.factory.annotation.Required;
 
-import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotBlank;
-import java.sql.Date;
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.sql.Timestamp;
 
 @Entity
 @Table(name = "bidlist")
+@Data
 public class BidList {
-    // TODO: Map columns in data table BIDLIST with corresponding java fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer bidListId;
+    private String account;
+    private String type;
+    private Double bidQuantity;
+    private Double askQuantity;
+    private Double bid;
+    private Double ask;
+    private String benchmark;
+    private Timestamp bidListDate;
+    private String commentary;
+    private String security;
+    private String status;
+    private String trader;
+    String book;
+    private String creationName;
+    private Timestamp creationDate;
+    String revisionName;
+    private Timestamp revisionDate;
+    private String dealName;
+    private String dealType;
+    private String sourceListId;
+    private String side;
 }
